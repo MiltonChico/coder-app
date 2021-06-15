30eeb3968735b9646a03374da8178a63472f778c
MiltonChico <milton.chico@outlook.com>
2021-06-15 12:39:04 -0300

    Corregido Contador de Stock

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
      <NavBar />
      <ItemListContainer greeting="Lista de Catalogo"/>
-     <ItemCount value={0}/>
+     <ItemCount stock={5} initial={1}/>
     </div>
   );
 }
--- src/components/common/ItemCount.js
+++ src/components/common/ItemCount.js
@@ -1,27 +1,36 @@
 import React, {useState} from 'react';
 
-const ItemCount  = ({ value }) => {
+const ItemCount  = ({stock, initial}) => {
 
-     const [ counter, setCounter ] = useState( value ); // []
+     const [ value, setValue ] = useState( initial ); // []
 
-     const handleAdd = () => {
-          setCounter ( counter + 1)
+     const onAdd = () => {
+          if (value >= 5 ){
+               alert('Superaste el stock de ' + stock)
+          } else {
+          setValue ( value + 1)
+          }
      }
 
-     const handleReset = () => {
-          setCounter ( value )
+     const onRest = () => {
+          if ( value <= 0){
+               alert ('No podes seleccionar menor cantidad')
+          } else {
+          setValue ( value -1 )
+          }
      }
 
-     const handleRest = () => {
-          setCounter (counter - 1)
+     const addToCart = () => {
+          alert('Agregado ' + value + ' al carrito ')
      }
+
      return (
           <>
                <h1>Carrito</h1>
-               <p>{counter}</p>
-               <button onClick={ handleRest }>-1</button>
-               <button onClick={ handleReset }>Reset</button>
-               <button onClick={ handleAdd }>+1</button>
+               <p>{value}</p>
+               <button onClick={ onRest }>-1</button>
+               <button onClick={ addToCart }>Agregar al Carrito</button>
+               <button onClick={ onAdd }>+1</button>
           </>
      );
 }
