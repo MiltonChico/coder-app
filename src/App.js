03b14d937a710dda771f30f90a129e7631c5d2ef
MiltonChico <milton.chico@outlook.com>
2021-07-06 22:53:59 -0300

    Test Context

--- src/App.js
+++ src/App.js
@@ -5,19 +5,22 @@ import NavBar from './components/common/NavBar'
 import ItemListContainer from './components/container/ItemListContainer';
 import ItemDetailContainer from './components/container/ItemDetailContainer'
 
+import ThemeContext from './context/context'
 
 function App() {
   return (
-    <BrowserRouter >
-      <NavBar />
-        <Switch > 
-            <Route exact path="/" component={ItemListContainer} />
-            <Route path="/buzos" component={ItemListContainer} />
-            <Route path="/pantalones" component={ItemListContainer} />
-            <Route path="/remeras" component={ItemListContainer} />
-            <Route path="/product/:id" component={ItemDetailContainer} />
-        </Switch>
-    </BrowserRouter >
+    <ThemeContext.Provider value={'test'}>
+      <BrowserRouter >
+        <NavBar />
+          <Switch > 
+              <Route exact path="/" component={ItemListContainer} />
+              <Route path="/buzos" component={ItemListContainer} />
+              <Route path="/pantalones" component={ItemListContainer} />
+              <Route path="/remeras" component={ItemListContainer} />
+              <Route path="/product/:id" component={ItemDetailContainer} />
+          </Switch>
+      </BrowserRouter >
+    </ThemeContext.Provider>
   );
 }
 
